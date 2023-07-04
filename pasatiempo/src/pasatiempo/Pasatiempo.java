3c373214073cac9a9bd9d43c0c4e2a73ac0b2fd1
EduardoCrPz <94722135+EduardoCrPz@users.noreply.github.com>
2023-07-04 11:40:33 -0600

    Add files via upload

---
+++ pasatiempo/src/pasatiempo/Pasatiempo.java
@@ -0,0 +1,24 @@
+
+package pasatiempo;
+
+import Views.View;
+
+
+
+
+/**
+ *
+ * @author lalo
+ */
+public class Pasatiempo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         View view = new View();
+         view.setVisible(true);
+        
+    }
+    
+}
